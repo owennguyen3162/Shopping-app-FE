72fac8c8a78cced95bc03611bfae05bd34ce6911
owennguyen3162 <nguyentuan3162@gmail.com>
2023-06-02 18:12:42 +0700

    Create Sweaters.js
    feature: setup-drawerNavigation

---
+++ src/Screen/clothes/Sweaters.js
@@ -0,0 +1,12 @@
+import {View, Text} from 'react-native';
+import React from 'react';
+
+const Sweaters = ({navigation}) => {
+  return (
+    <View>
+      <Text>Sweaters</Text>
+    </View>
+  );
+};
+
+export default Sweaters;
