1fd9c295558065aacc1da4c26f5d6a0309630f30
owennguyen3162 <nguyentuan3162@gmail.com>
2023-06-02 20:53:45 +0700

    Update index.js
    feature/UI-Screen

--- src/Screen/Home/index.js
+++ src/Screen/Home/index.js
@@ -1,32 +1,52 @@
 import * as React from 'react';
-import {View, Text, Button, Image, StyleSheet} from 'react-native';
+import {View, Text, Button, Image, StyleSheet, StatusBar} from 'react-native';
 import {
   createDrawerNavigator,
   DrawerContentScrollView,
   DrawerItemList,
   DrawerItem,
+  useDrawerProgress,
 } from '@react-navigation/drawer';
 import Pants from '../clothes/Pants';
-import Sweaters from '../clothes/Sweaters';
 import {ScreenOption} from '../../utils/ScreenOptions';
+import Account from '../Account';
+import Sweaters from '../clothes/Sweaters';
 import Accesories from '../clothes/Accesories';
 import Shoes from '../clothes/Shoes';
 import Underwear from '../clothes/Underwear';
+import Animated from 'react-native-reanimated';
 
 const CustomDrawerContent = props => {
+  const progress = useDrawerProgress();
+
+  // If you are on react-native-reanimated 1.x, use `Animated.interpolate` instead of `Animated.interpolateNode`
+  const translateX = Animated.interpolateNode(progress, {
+    inputRange: [0, 1],
+    outputRange: [-100, 0],
+  });
   return (
     <DrawerContentScrollView {...props} style={Style.drawContainer}>
-      <View style={Style.childDrawer}>
-        <Image
-          source={require('../../public/image/avata.png')}
-          style={Style.image}
+      <Animated.View style={{transform: [{translateX}]}}>
+        <View style={Style.childDrawer}>
+          <Image
+            source={require('../../public/image/avata.png')}
+            style={Style.image}
+          />
+        </View>
+        <DrawerItemList {...props} />
+        <DrawerItem
+          label="Close"
+          onPress={() => props.navigation.toggleDrawer()}
         />
-      </View>
-      <DrawerItemList {...props} />
-      <DrawerItem
-        label="Close"
-        onPress={() => props.navigation.toggleDrawer()}
-      />
+        <View style={Style.bottomDrawer}>
+          <Image
+            source={{
+              uri: 'https://cdn-icons-png.flaticon.com/512/1687/1687788.png',
+            }}
+            style={Style.light_bulb}
+          />
+        </View>
+      </Animated.View>
     </DrawerContentScrollView>
   );
 };
@@ -43,6 +63,7 @@ function Home() {
       <Drawer.Screen name="Accesories" component={Accesories} />
       <Drawer.Screen name="Shoes" component={Shoes} />
       <Drawer.Screen name="Underwear" component={Underwear} />
+      <Drawer.Screen name="Account" component={Account} />
     </Drawer.Navigator>
   );
 }
@@ -52,9 +73,28 @@ const Style = StyleSheet.create({
   },
   drawContainer: {
     paddingHorizontal: 10,
+    flex: 1,
   },
   childDrawer: {
     alignItems: 'center',
+  },
+  light_bulb: {
+    width: 20,
+    height: 20,
+    resizeMode: 'center',
+  },
+
+  bottomDrawer: {
+    alignItems: 'center',
+    marginRight: 20,
+    justifyContent: 'center',
+    width: 30,
+    height: 30,
+    borderRadius: 50,
+    backgroundColor: '#EEEEEE',
+    elevation: 1,
+    marginTop: 30,
+    marginBottom: 20,
   },
 });
 export default Home;
