d3f442252399a6c5470b860878dcbc7570b796c2
owennguyen3162 <nguyentuan3162@gmail.com>
2023-06-03 13:42:14 +0700

    Update Accesories.js
    feature/UI-Screen

--- src/Screen/clothes/Accesories.js
+++ src/Screen/clothes/Accesories.js
@@ -5,12 +5,11 @@ import {
   StatusBar,
   StyleSheet,
   Image,
-  TouchableHighlight,
-  TouchableWithoutFeedback,
+  Pressable,
   FlatList,
+  RefreshControl,
 } from 'react-native';
 import React from 'react';
-import {TouchableOpacity} from 'react-native-gesture-handler';
 import Item from '../../components/Item';
 
 const Accesories = ({navigation}) => {
@@ -51,33 +50,83 @@ const Accesories = ({navigation}) => {
       description:
         'The Milford Beanie is a 100% acrylic slouch beanie with an old school Vans OTW clip label.',
     },
+    {
+      id: 5,
+      image:
+        'https://cdn.shopify.com/s/files/1/0613/7695/4559/products/pixelcut-export-1679479942753_7a99f9d2-bb3a-4848-bdb1-0b6520ba1a0d_1800x1800.png?v=1681602819',
+      price: 123,
+      name: 'Milford beanie',
+      description:
+        'The Milford Beanie is a 100% acrylic slouch beanie with an old school Vans OTW clip label.',
+    },
+    {
+      id: 6,
+      image:
+        'https://cdn.shopify.com/s/files/1/0613/7695/4559/products/pixelcut-export-1679479942753_7a99f9d2-bb3a-4848-bdb1-0b6520ba1a0d_1800x1800.png?v=1681602819',
+      price: 123,
+      name: 'Milford beanie',
+      description:
+        'The Milford Beanie is a 100% acrylic slouch beanie with an old school Vans OTW clip label.',
+    },
+    {
+      id: 7,
+      image:
+        'https://cdn.shopify.com/s/files/1/0613/7695/4559/products/pixelcut-export-1679479942753_7a99f9d2-bb3a-4848-bdb1-0b6520ba1a0d_1800x1800.png?v=1681602819',
+      price: 123,
+      name: 'Milford beanie',
+      description:
+        'The Milford Beanie is a 100% acrylic slouch beanie with an old school Vans OTW clip label.',
+    },
+    {
+      id: 8,
+      image:
+        'https://cdn.shopify.com/s/files/1/0613/7695/4559/products/pixelcut-export-1679479942753_7a99f9d2-bb3a-4848-bdb1-0b6520ba1a0d_1800x1800.png?v=1681602819',
+      price: 123,
+      name: 'Milford beanie',
+      description:
+        'The Milford Beanie is a 100% acrylic slouch beanie with an old school Vans OTW clip label.',
+    },
   ];
+  const [refreshing, setRefreshing] = React.useState(false);
+
+  const onRefresh = React.useCallback(() => {
+    setRefreshing(true);
+    setTimeout(() => {
+      setRefreshing(false);
+    }, 2000);
+  }, []);
   return (
     <View style={Style.container}>
       <StatusBar hidden={true} />
       <View style={Style.header}>
         <Text style={Style.text}>Accesories</Text>
-        <TouchableWithoutFeedback onPress={() => navigation.toggleDrawer()}>
+        <Pressable onPress={() => navigation.toggleDrawer()}>
           <Image
             source={{
               uri: 'https://cdn-icons-png.flaticon.com/128/1828/1828859.png',
             }}
             style={Style.image}
           />
-        </TouchableWithoutFeedback>
+        </Pressable>
       </View>
-      <View style={{width: '100%'}}>
+      <View style={{width: '100%', height: '91%'}}>
         <FlatList
+          showsVerticalScrollIndicator={false}
           initialNumToRender={10}
           keyExtractor={item => item.id}
           data={FakeData}
+          refreshControl={
+            <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+          }
           renderItem={({item}) => (
-            <Item
-              image={item.image}
-              name={item.name}
-              description={item.description}
-              price={item.price}
-            />
+            <Pressable onPress={() => navigation.navigate('ProductDetail')}>
+              <Item
+                image={item.image}
+                name={item.name}
+                description={item.description}
+                price={item.price}
+              />
+            </Pressable>
           )}
         />
       </View>
