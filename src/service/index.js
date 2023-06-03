4a3ba17af1abf47feddee75534fd88ead15c2c8e
owennguyen3162 <nguyentuan3162@gmail.com>
2023-06-03 23:51:04 +0700

    Create Service
    feature/switch-Color

---
+++ src/service/index.js
@@ -0,0 +1,20 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+export const setColorToStorage = async color => {
+  try {
+    await AsyncStorage.setItem('Color', color);
+  } catch (error) {
+    console.log('Set error::: ' + error);
+  }
+};
+
+export const getColorToStorage = async () => {
+  try {
+    const data = await AsyncStorage.getItem('Color');
+    if (data) {
+      return data;
+    }
+  } catch (error) {
+    return 'white';
+  }
+};
