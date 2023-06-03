08a2d5731cc2643149ec60abc2ef65265846bd95
owennguyen3162 <nguyentuan3162@gmail.com>
2023-06-03 23:47:58 +0700

    Update index.js
    feature/switch-Color

--- src/components/Item/index.js
+++ src/components/Item/index.js
@@ -1,23 +1,30 @@
 import {View, Text, StyleSheet, Image} from 'react-native';
 import React from 'react';
-
+import {useSelector} from 'react-redux';
 const Item = props => {
   const {name, price, description, image} = props;
+  const theme = useSelector(state => state.SwitchColor);
   return (
-    <View style={Style.container}>
+    <View
+      style={theme.color === 'white' ? Style.container : Style.containerDark}>
       <Image source={{uri: image}} style={Style.image} />
-      <View style={Style.line}></View>
       <View
-        style={{
-          height: '80%',
-          justifyContent: 'space-between',
-          marginLeft: 15,
-        }}>
-        <Text style={{color: 'black', fontWeight: 'bold'}}>{name}</Text>
-        <Text style={{width: '45%', fontSize: 12.5, color: 'black'}}>
+        style={theme.color === 'white' ? Style.line : Style.lineDark}></View>
+      <View style={Style.body}>
+        <Text
+          style={theme.color === 'white' ? Style.TextName : Style.TextNameDark}>
+          {name}
+        </Text>
+        <Text
+          style={theme.color === 'white' ? Style.textDesc : Style.textDescDark}>
           {description}
         </Text>
-        <Text style={{color: 'black', fontWeight: 'bold'}}>$ {price}</Text>
+        <Text
+          style={
+            theme.color === 'white' ? Style.textPrice : Style.textPriceDark
+          }>
+          $ {price}
+        </Text>
       </View>
     </View>
   );
@@ -36,6 +43,17 @@ const Style = StyleSheet.create({
     elevation: 3,
     backgroundColor: 'white',
   },
+  containerDark: {
+    width: '100%',
+    height: 120,
+    borderWidth: 1,
+    borderRadius: 5,
+    marginVertical: 8,
+    flexDirection: 'row',
+    alignItems: 'center',
+    elevation: 3,
+    backgroundColor: '#222222',
+  },
   image: {
     width: 100,
     height: 100,
@@ -48,6 +66,23 @@ const Style = StyleSheet.create({
     borderColor: 'silver',
     marginLeft: 15,
   },
+  lineDark: {
+    height: '80%',
+    borderWidth: 0.5,
+    borderColor: 'white',
+    marginLeft: 15,
+  },
+  body: {
+    height: '80%',
+    justifyContent: 'space-between',
+    marginLeft: 15,
+  },
+  TextName: {color: 'black', fontWeight: 'bold'},
+  TextNameDark: {color: 'white', fontWeight: 'bold'},
+  textDesc: {width: '45%', fontSize: 12.5, color: 'black'},
+  textDescDark: {width: '45%', fontSize: 12.5, color: 'white'},
+  textPrice: {color: 'black', fontWeight: 'bold'},
+  textPriceDark: {color: 'white', fontWeight: 'bold'},
 });
 
 export default Item;
