41a512b0d9c73563af4f94630db6819a5b85e422
owennguyen3162 <nguyentuan3162@gmail.com>
2023-06-03 18:26:19 +0700

    Create Reducer
    feature/setup-redux

---
+++ src/redux/reducers/index.js
@@ -0,0 +1,8 @@
+import {combineReducers} from 'redux';
+
+import switchColor from './switchColor';
+const Reducer = combineReducers({
+  SwitchColor: switchColor,
+});
+
+export default (state, action) => Reducer(state, action);
