472fefe851bd1d634d11a32ca3cf9e0ff0535995
owennguyen3162 <nguyentuan3162@gmail.com>
2023-06-07 10:20:05 +0700

    Update axios.js
    feature/api-server-authentication

--- src/service/axios.js
+++ src/service/axios.js
@@ -1,50 +1,61 @@
 import axios from 'axios';
-
+import {
+  getLocalAccessToken,
+  getLocalRefreshToken,
+  updateLocalAccessToken,
+} from './token.service';
 const instance = axios.create({
   baseURL: 'http://192.168.0.101:3000',
   headers: {
     'Content-Type': 'application/json',
   },
 });
-// instance.interceptors.request.use(
-//   async (config) => {
-//     const token = await TokenService.getLocalAccessToken();
-//     if (token) {
-//       config.headers["x_authorization"] = token;
-//     }
-//     return config;
-//   },
-//   (error) => {
-//     console.log("REQUEST ::::" + error.response.status);
-//     return Promise.reject(error);
-//   }
-// );
 
-// instance.interceptors.response.use(
-//   (res) => {
-//     return res;
-//   },
-//   async (err) => {
-//     const originalConfig = err.config;
-//     if (originalConfig.url !== "/auth/login" && err.response) {
-//       // Access Token was expired
-//       if (err.response.status === 401 && !originalConfig._retry) {
-//         originalConfig._retry = true;
-//         const RefreshToken = await TokenService.getLocalRefreshToken();
-//         try {
-//           const rs = await instance.post("/auth/refresh", {
-//             refreshToken: RefreshToken,
-//           });
-//           const { accessToken } = rs.data;
-//           await TokenService.updateLocalAccessToken(accessToken);
-//           return instance(originalConfig);
-//         } catch (_error) {
-//           return Promise.reject(_error);
-//         }
-//       }
-//     }
+instance.interceptors.request.use(
+  async config => {
+    const token = await getLocalAccessToken();
 
-//     return Promise.reject(err);
-//   }
-// );
+    if (token) {
+      config.headers['x_authorization'] = token;
+    }
+    return config;
+  },
+  error => {
+    return Promise.reject(error);
+  },
+);
+
+instance.interceptors.response.use(
+  res => {
+    return res;
+  },
+  async err => {
+    const originalConfig = err.config;
+    if (
+      originalConfig.url !== '/api/user/login' &&
+      '/api/user/createAccount' &&
+      '/auth/refreshToken' &&
+      err.response
+    ) {
+      // Access Token was expired
+      if (err.response.status === 401 && !originalConfig._retry) {
+        originalConfig._retry = true;
+        const RefreshToken = await getLocalRefreshToken();
+        try {
+          const rs = await instance.post('/auth/refreshToken', {
+            refreshToken: RefreshToken,
+            refreshTokenToClient: RefreshToken,
+          });
+          const {accessToken} = rs.data;
+          await updateLocalAccessToken(accessToken);
+          return instance(originalConfig);
+        } catch (_error) {
+          return Promise.reject(_error);
+        }
+      }
+    }
+
+    return Promise.reject(err);
+  },
+);
 export default instance;
