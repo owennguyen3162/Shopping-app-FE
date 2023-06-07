483fba2efdd2df746ae8139f6e55ce26e6e1baae
owennguyen3162 <nguyentuan3162@gmail.com>
2023-06-07 10:18:53 +0700

    Update index.js
    feature/api-server-authentication

--- src/navigation/MainStack/index.js
+++ src/navigation/MainStack/index.js
@@ -3,20 +3,22 @@ import {getColorToStorage} from '../../service';
 import AuthStack from '../Auth';
 import HomeStack from '../Home';
 import {SwitchColor} from '../../redux/action/changeColor';
-import {useDispatch} from 'react-redux';
-const isLogined = true;
+import {useDispatch, useSelector} from 'react-redux';
 const MainStack = () => {
+  const isLoginess = useSelector(state => state.Auth);
+
   const dispatch = useDispatch();
   React.useEffect(() => {
     getTheme();
-  });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   const getTheme = async () => {
     const value = await getColorToStorage();
     return dispatch(SwitchColor(value));
   };
 
-  return !isLogined ? <AuthStack /> : <HomeStack />;
+  return !isLoginess.isLoginess ? <AuthStack /> : <HomeStack />;
 };
 
 export default MainStack;
