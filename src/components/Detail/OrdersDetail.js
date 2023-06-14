444de19cf579107ab7b4d697ff48f644177b3ea6
owennguyen3162 <nguyentuan3162@gmail.com>
2023-06-14 16:55:08 +0700

    Update OrdersDetail.js

--- src/components/Detail/OrdersDetail.js
+++ src/components/Detail/OrdersDetail.js
@@ -1,7 +1,7 @@
 import {View, Text, StyleSheet, Image, FlatList, Pressable} from 'react-native';
 import React from 'react';
 import {useSelector} from 'react-redux';
-import Item from '../Item';
+import Item from '../Item/Item';
 
 const OrdersDetail = ({route, navigation}) => {
   const {data, totalPrice, code, address, time} = route.params;
