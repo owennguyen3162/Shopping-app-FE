4396d5a1548717c053dba517ef16193efc0f0168
owennguyen3162 <nguyentuan3162@gmail.com>
2023-06-15 00:06:57 +0700

    Update types.js

--- src/redux/action/types.js
+++ src/redux/action/types.js
@@ -4,3 +4,5 @@ export const FETCH_FAIL = 'FETCH_FAIL';
 export const CHANGE_COLOR = 'CHANGE_COLOR';
 export const LOGIN = 'LOGIN';
 export const LOGIN_OUT = 'LOGIN_OUT';
+export const HIDDEN_TABBAR = 'HIDDEN_TABBAR';
+export const SHOW_TABAR = 'SHOW_TABAR';
