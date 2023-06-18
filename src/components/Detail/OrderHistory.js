e3fe31e2a8930673a9aca6aaf57bf269608cb2a9
owennguyen3162 <nguyentuan3162@gmail.com>
2023-06-18 21:18:57 +0700

    Update OrderHistory.js

--- src/components/Detail/OrderHistory.js
+++ src/components/Detail/OrderHistory.js
@@ -1,8 +1,44 @@
-import {View, Text, StyleSheet, Image, Pressable} from 'react-native';
-import React from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+  Image,
+  Pressable,
+  FlatList,
+  Alert,
+  ActivityIndicator,
+} from 'react-native';
+import React, {useEffect} from 'react';
 import {useSelector} from 'react-redux';
+import instance from '../../service/axios';
+import {getUserId} from '../../service/user.service';
+import ItemOrderHistory from '../Item/ItemOrderHistory';
 const OrderHistory = ({navigation}) => {
   const theme = useSelector(theme => theme.SwitchColor);
+  const [userId, setUserId] = React.useState(null);
+  const [data, setData] = React.useState([]);
+  const [isLoading, setIsLoading] = React.useState(true);
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  const getData = async () => {
+    try {
+      const data = await instance.get(
+        `/api/order/history/${await getUserId()}`,
+      );
+      if (data.status === 200) {
+        setData(data.data.data);
+      }
+    } catch (error) {
+      Alert.alert('Notification', 'Get data fail', [
+        {text: 'OK', style: 'cancel'},
+      ]);
+    } finally {
+      setIsLoading(false);
+    }
+  };
   return (
     <View
       style={theme.color === 'white' ? Style.container : Style.containerDark}>
@@ -14,6 +50,25 @@ const OrderHistory = ({navigation}) => {
           style={theme.color === 'white' ? Style.icon : Style.iconDark}
         />
       </Pressable>
+      {isLoading ? (
+        <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+          <ActivityIndicator />
+        </View>
+      ) : (
+        <View style={{flex: 1, width: '100%', marginTop: 30}}>
+          <FlatList
+            keyExtractor={item => item.id}
+            data={data}
+            renderItem={({item}) => (
+              <ItemOrderHistory
+                price={item.totalPrice}
+                date={item.date}
+                quantity={JSON.parse(item.cartItem).length}
+              />
+            )}
+          />
+        </View>
+      )}
     </View>
   );
 };
