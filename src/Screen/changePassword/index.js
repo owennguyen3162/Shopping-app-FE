1c15f762f09e2695fc5f114c30b6d3a4aa525c5f
owennguyen3162 <nguyentuan3162@gmail.com>
2023-06-19 00:15:49 +0700

    Create index.js

---
+++ src/Screen/changePassword/index.js
@@ -0,0 +1,138 @@
+import {View, Text, StyleSheet, Image, Pressable, Alert} from 'react-native';
+import React from 'react';
+import {useSelector} from 'react-redux';
+import instance from '../../service/axios';
+import {getUserId} from '../../service/user.service';
+import {TextInput} from 'react-native-paper';
+
+const ChangePassword = ({navigation}) => {
+  const theme = useSelector(theme => theme.SwitchColor);
+  const [password, setPassword] = React.useState('');
+  const [verify, setVerify] = React.useState('');
+  const [userId, setUserId] = React.useState(null);
+  const [passwordStatus, setPasswordStatus] = React.useState(false);
+  const [verifyStatus, setVerifyStatus] = React.useState(false);
+
+  const handleEdit = async () => {
+    try {
+      const data = await instance.put(`/api/user/changePassword/${userId}`);
+      if (data.status === 200) {
+        Alert.alert(
+          'Notification',
+          'Password changed successfully, please login again',
+          [{text: 'OK', style: 'cancel', onPress: () => navigation.goBack()}],
+        );
+      }
+    } catch (error) {
+      Alert.alert('Notification', 'Edit fail', [{text: 'OK', style: 'cancel'}]);
+    }
+  };
+
+  React.useEffect(() => {
+    getUserId()
+      .then(id => setUserId(id))
+      .catch(error => console.log(error));
+  }, []);
+
+  return (
+    <View
+      style={theme.color === 'white' ? Style.container : Style.containerDark}>
+      <View style={Style.header}>
+        <Pressable onPress={() => navigation.goBack()}>
+          <Image
+            source={{
+              uri: 'https://cdn-icons-png.flaticon.com/128/2732/2732652.png',
+            }}
+            style={theme.color === 'white' ? Style.icon : Style.iconDark}
+          />
+        </Pressable>
+
+        <Text style={theme.color === 'white' ? Style.title : Style.titleDark}>
+          CHANGE PASSWORD
+        </Text>
+
+        <Pressable onPress={() => handleEdit()}>
+          <Image
+            source={{
+              uri: 'https://cdn-icons-png.flaticon.com/128/5996/5996831.png',
+            }}
+            style={Style.iconEditDark}
+            resizeMode="center"
+          />
+        </Pressable>
+      </View>
+      <View
+        style={{marginTop: 30, flex: 1, width: '100%', alignItems: 'center'}}>
+        <TextInput
+          secureTextEntry={passwordStatus ? false : true}
+          mode="outlined"
+          right={
+            <TextInput.Icon
+              icon={passwordStatus ? 'eye' : 'eye-off'}
+              onPress={() => setPasswordStatus(!passwordStatus)}
+            />
+          }
+          style={
+            theme.color === 'white' ? Style.textInput : Style.textInputDark
+          }
+          placeholder="Password"
+          textColor={theme.color === 'white' ? 'black' : 'white'}
+        />
+        <TextInput
+          secureTextEntry={verifyStatus ? false : true}
+          mode="outlined"
+          right={
+            <TextInput.Icon
+              icon={verifyStatus ? 'eye' : 'eye-off'}
+              onPress={() => setVerifyStatus(!verifyStatus)}
+            />
+          }
+          style={
+            theme.color === 'white' ? Style.textInput : Style.textInputDark
+          }
+          textColor={theme.color === 'white' ? 'black' : 'white'}
+          placeholder="Verify"
+        />
+      </View>
+    </View>
+  );
+};
+const Style = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    paddingHorizontal: 15,
+    backgroundColor: 'white',
+  },
+  containerDark: {
+    flex: 1,
+    alignItems: 'center',
+    paddingHorizontal: 15,
+    backgroundColor: '#111111',
+  },
+  icon: {width: 35, height: 35},
+  iconDark: {width: 35, height: 35, tintColor: 'white'},
+  iconEditDark: {width: 40, height: 40},
+  title: {fontWeight: 'bold', fontSize: 17, color: 'black'},
+  titleDark: {fontWeight: 'bold', fontSize: 17, color: 'white'},
+  textInput: {
+    width: '100%',
+    backgroundColor: 'white',
+    color: 'white',
+    marginVertical: 8,
+  },
+  textInputDark: {
+    width: '100%',
+    backgroundColor: 'black',
+    color: 'white',
+    marginVertical: 8,
+  },
+  header: {
+    marginTop: 20,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    width: '100%',
+  },
+});
+export default ChangePassword;
