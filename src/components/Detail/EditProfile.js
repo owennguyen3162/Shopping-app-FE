f5e6b32dabaad45002ac53a435d5b3dbdb4e667d
owennguyen3162 <nguyentuan3162@gmail.com>
2023-06-18 20:04:25 +0700

    Update EditProfile.js

--- src/components/Detail/EditProfile.js
+++ src/components/Detail/EditProfile.js
@@ -3,26 +3,68 @@ import {
   Text,
   StyleSheet,
   Image,
-  ImageBackground,
   TextInput,
   Pressable,
+  Alert,
 } from 'react-native';
 import React from 'react';
 import {useSelector} from 'react-redux';
+import {launchImageLibrary} from 'react-native-image-picker';
+import instance from '../../service/axios';
+import {getUserId} from '../../service/user.service';
+import axios from 'axios';
+
 const EditProfile = ({navigation, route}) => {
   const theme = useSelector(theme => theme.SwitchColor);
   const {name, phone, address, image} = route.params;
+  const [Name, setName] = React.useState(name);
+  const [Address, setAdress] = React.useState(address);
+  const [imageLib, setImage] = React.useState(image);
+  const [userId, setUserId] = React.useState(null);
+
+  const openCamera = async () => {
+    const options = {
+      mediaType: 'photo',
+      includeBase64: false,
+      quality: 1,
+    };
+    const result = await launchImageLibrary(options);
+    if (result.assets) {
+      setImage(result.assets[0].uri);
+    }
+  };
+
+  const handleEdit = async () => {
+    const formData = new FormData();
+    formData.append('name', Name);
+    formData.append('address', Address);
+    formData.append('userImage', {
+      uri: imageLib,
+      name: 'imageFromMobile.jpg',
+      type: 'image/jpg',
+    });
+    try {
+      const data = await instance.put(`/api/user/${userId}`, formData, {
+        headers: {'Content-Type': 'multipart/form-data'},
+      });
+      if(data.status === 200){
+        Alert.alert('Notification', 'Edit successfully', [{text: 'OK', style: 'cancel', onPress: () => navigation.goBack()}]);
+      }
+    } catch (error) {
+      Alert.alert('Notification', 'Edit fail', [{text: 'OK', style: 'cancel'}]);
+    }
+  };
+
+  React.useEffect(() => {
+    getUserId()
+      .then(res => setUserId(res))
+      .catch(error => console.log(error));
+  }, []);
+
   return (
     <View
       style={theme.color === 'white' ? Style.container : Style.containerDark}>
-      <View
-        style={{
-          marginTop: 20,
-          flexDirection: 'row',
-          justifyContent: 'space-between',
-          alignItems: 'center',
-          width: '100%',
-        }}>
+      <View style={Style.header}>
         <Pressable onPress={() => navigation.goBack()}>
           <Image
             source={{
@@ -36,45 +78,52 @@ const EditProfile = ({navigation, route}) => {
           EDIT PROFILE
         </Text>
 
-        <Image
-          source={{
-            uri: 'https://cdn-icons-png.flaticon.com/128/5996/5996831.png',
-          }}
-          style={Style.iconEditDark}
-          resizeMode="center"
-        />
+        <Pressable onPress={() => handleEdit()}>
+          <Image
+            source={{
+              uri: 'https://cdn-icons-png.flaticon.com/128/5996/5996831.png',
+            }}
+            style={Style.iconEditDark}
+            resizeMode="center"
+          />
+        </Pressable>
       </View>
       <View
         style={{marginTop: 30, flex: 1, width: '100%', alignItems: 'center'}}>
-        <Image
-          source={{
-            uri: image,
-          }}
-          style={{
-            width: 150,
-            height: 150,
-            borderRadius: 100,
-          }}
-        />
+        <Pressable onPress={() => openCamera()}>
+          <Image
+            source={{
+              uri: imageLib,
+            }}
+            style={{
+              width: 150,
+              height: 150,
+              borderRadius: 100,
+            }}
+          />
+        </Pressable>
         <TextInput
           style={
             theme.color === 'white'
-              ? [Style.textInput, {marginTop: 20}]
-              : [Style.textInputDark, {marginTop: 20}]
+              ? [Style.textInput, {marginTop: 20, color: 'black'}]
+              : [Style.textInputDark, {marginTop: 20, color: 'white'}]
           }
           value={phone}
+          editable={false}
         />
         <TextInput
           style={
             theme.color === 'white' ? Style.textInput : Style.textInputDark
           }
-          value={name}
+          value={Name}
+          onChangeText={text => setName(text)}
         />
         <TextInput
           style={
             theme.color === 'white' ? Style.textInput : Style.textInputDark
           }
-          value={address}
+          value={Address}
+          onChangeText={text => setAdress(text)}
         />
       </View>
     </View>
@@ -105,6 +154,7 @@ const Style = StyleSheet.create({
     borderColor: 'silver',
     borderRadius: 10,
     marginVertical: 8,
+    paddingLeft: 10,
   },
   textInputDark: {
     width: '100%',
@@ -115,6 +165,13 @@ const Style = StyleSheet.create({
     marginVertical: 8,
     color: 'white',
     paddingLeft: 10,
+  },
+  header: {
+    marginTop: 20,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    width: '100%',
   },
 });
 export default EditProfile;
