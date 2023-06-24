779f210124b520108bcc4525eb21c950b83b9925
owennguyen3162 <nguyentuan3162@gmail.com>
2023-06-25 01:21:28 +0700

    Update index.js

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -4,12 +4,14 @@ import SwitchColor from './switchColor';
 import Auth from './auth';
 import tabbarStatus from './tabbarStatus';
 import cart from './cart';
+import order from './order';
 
 const Reducer = combineReducers({
   SwitchColor: SwitchColor,
   Auth: Auth,
   tabbarStatus: tabbarStatus,
   Cart: cart,
+  Order: order,
 });
 
 export default (state, action) => Reducer(state, action);
