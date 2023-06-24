e4a6344b292818203be225ff0a777cbe11e85219
owennguyen3162 <nguyentuan3162@gmail.com>
2023-06-24 14:11:16 +0700

    Update types.js

--- src/redux/action/types.js
+++ src/redux/action/types.js
@@ -6,3 +6,10 @@ export const LOGIN = 'LOGIN';
 export const LOGIN_OUT = 'LOGIN_OUT';
 export const HIDDEN_TABBAR = 'HIDDEN_TABBAR';
 export const SHOW_TABAR = 'SHOW_TABAR';
+export const COUNT = 'COUNT';
+export const REDUCE = 'REDUCE';
+export const LENGHT = 'LENGHT';
+export const CHECK_OUT = 'CHECK_OUT';
+
+
+
